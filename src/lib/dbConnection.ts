4d88389388ae18069f0155bd8e84461838b1d06b
roghelia <dannyroghs1@gmail.com>
2024-06-06 20:01:18 +0530

    created database connection.

---
+++ src/lib/dbConnection.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+    isConnected?: number,
+}
+
+const connection: connectionObject = {};
+
+async function dbConnect():Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected..");   
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+        connection.isConnected = db.connections[0].readyState;
+    
+        console.log("Connected to MongoDB successfully.");
+    } catch (error) {
+        console.log("MongoDB error: ", error)
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
