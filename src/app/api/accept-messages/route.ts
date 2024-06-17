eb74303b70da571b67ca056d79813b3dea906e44
roghelia <dannyroghs1@gmail.com>
2024-06-17 11:42:29 +0530

    more APIs.

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,64 @@
+import { getServerSession, User } from "next-auth";
+import { AuthOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnection";
+import userModel from "@/models/User";
+export async function POST(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(AuthOptions);
+    const user: User = session?.user as User;
+
+    if(!session || !session.user) {
+        return Response.json({
+            success: false, 
+            message: "You must be logged in to accept messages."
+        }, {status: 401})
+    }
+    const userId = user._id;
+    const { acceptMessages } = await request.json();
+
+    try {
+        return Response.json({
+            success: true,
+            message: "Messages accepted successfully.",
+        }, {status: 200})
+    } catch (error) {
+        return Response.json({
+            success: false, 
+            message: "An error occurred while accepting messages."
+        }, {status: 500})
+    }
+}
+
+export async function GET(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(AuthOptions);
+    const user: User = session?.user as User;
+
+    if(!session || !session.user) {
+        return Response.json({
+            success: false, 
+            message: "You must be logged in to accept messages."
+        }, {status: 401})
+    }
+    const userId = user._id;
+    try {
+        const foundUser = await userModel.findById(userId);
+        if(!foundUser) {
+            return Response.json({
+                success: false, 
+                message: "User not found."
+            }, {status: 404})
+        }
+        return Response.json({
+            success: true,
+            message: "Messages accepted successfully.",
+            isAcceptingMessages: foundUser.isAcceptingMessage,
+            // isVerified: foundUser.isVerified
+        }, {status: 200})
+    } catch (error) {
+        return Response.json({
+            success: false, 
+            message: "An error occurred while accepting messages."
+        }, {status: 500})
+    }
+}
